ca82f4d9d7da386ee6f063f2fec6deecbc993d95
psanchez <pablo.sanchez@teltek.es>
2026-07-22 09:44:27 +0200

    Primeros Dotfiles

---
+++ bootstrap.sh
@@ -0,0 +1,15 @@
+sudo apt update
+
+sudo apt install -y \
+    git \
+    curl \
+    wget \
+    zsh \
+    eza \
+    bat \
+    fzf \
+    zoxide \
+    htop \
+    btop \
+    unzip \
+    ripgrep
